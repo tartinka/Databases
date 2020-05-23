cef17023066afba4324f6f76444f3e7772e5e27a
Olga Tinyakova <tartinka@mail.ru>
2020-05-23 12:56:57 +0300

    Задания к 7 уроку

---
+++ Lesson7/lesson7.sql
@@ -0,0 +1,184 @@
+CREATE DATABASE store;
+USE store;
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+DROP TABLE IF EXISTS rubrics;
+CREATE TABLE rubrics (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела'
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO rubrics VALUES
+  (NULL, 'Видеокарты'),
+  (NULL, 'Память');
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
+) COMMENT = 'Заказы';
+
+DROP TABLE IF EXISTS orders_products;
+CREATE TABLE orders_products (
+  id SERIAL PRIMARY KEY,
+  order_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Состав заказа';
+
+DROP TABLE IF EXISTS discounts;
+CREATE TABLE discounts (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
+  started_at DATETIME,
+  finished_at DATETIME,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id),
+  KEY index_of_product_id(product_id)
+) COMMENT = 'Скидки';
+
+DROP TABLE IF EXISTS storehouses;
+CREATE TABLE storehouses (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Склады';
+
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+  id SERIAL PRIMARY KEY,
+  storehouse_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+
+-- Задание №1 Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+
+SELECT DISTINCT
+    name
+FROM
+    users
+        JOIN
+    orders
+WHERE
+    users.id = orders.user_id;
+
+-- Задание №2 Выведите список товаров products и разделов catalogs, который соответствует товару.
+
+SELECT 
+    products.name, catalogs.name
+FROM
+    products
+        JOIN
+    catalogs
+WHERE
+    products.catalog_id = catalogs.id;
+
+-- Задание №3 (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
+-- Поля from, to и label содержат английские названия городов, поле name — русское. 
+-- Выведите список рейсов flights с русскими названиями городов.
+
+CREATE DATABASE flights_schedule;
+use flights_schedule;
+DROP TABLE flights;
+CREATE TABLE flights (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `from` VARCHAR(50) NOT NULL,
+  `to` VARCHAR(50) NOT NULL
+);
+
+INSERT INTO flights (id, `from`, `to`) VALUES ('1', 'moscow', 'omsk');
+INSERT INTO flights (id, `from`, `to`) VALUES ('2', 'novgorod', 'kazan');
+INSERT INTO flights (id, `from`, `to`) VALUES ('3', 'irkutsk', 'moscow');
+INSERT INTO flights (id, `from`, `to`) VALUES ('4', 'omsk', 'irkutsk');
+INSERT INTO flights (id, `from`, `to`) VALUES ('5', 'moscow', 'kazan');
+
+
+CREATE TABLE cities (
+  label VARCHAR(50) NOT NULL PRIMARY KEY,
+  `name` VARCHAR(50) NOT NULL UNIQUE
+);
+INSERT INTO cities (label, `name`) VALUES ('moscow', 'Москва');
+INSERT INTO cities (label, `name`) VALUES ('irkutsk', 'Иркутск');
+INSERT INTO cities (label, `name`) VALUES ('novgorod', 'Новгород');
+INSERT INTO cities (label, `name`) VALUES ('kazan', 'Казань');
+INSERT INTO cities (label, `name`) VALUES ('omsk', 'Омск');
+
+SELECT 
+    id, flights_new.from, cities.name
+FROM
+    (SELECT 
+        id, `name` AS `from`, `to`
+    FROM
+        flights
+    JOIN cities
+    WHERE
+        flights.from = cities.label) AS flights_new
+        JOIN
+    cities
+WHERE
+    flights_new.to = cities.label;
